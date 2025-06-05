c2441186de27563d96be7441733893ce57ef2a4c
pedroalma <pedroalma554@gmail.com>
2025-06-05 10:39:17 -0300

    1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
     <div className='app'>
-      <Tudo foto={f1} nome="neymar" numero="10" posicao="atacante" habilidade="dribles, passes precisos e finalizações com ambas as pernas"/>
-      <Tudo foto={f2} nome="Joaquim" numero="2" posicao="Zagueiro central" habilidade="rapidez de raciocínio e inteligência na armação das jogadas"/>
-      <Tudo foto={f3} nome="pituca" numero="21" posicao="Meio-campista" habilidade="rapidez de raciocínio e inteligência"/>
+      <Tudo foto={f1} nome="Neymar" numero="10" posicao="atacante" time="Santos" habilidade="dribles, passes precisos e finalizações com ambas as pernas."/>
+      <Tudo foto={f2} nome="Joaquim" numero="2" posicao="Zagueiro central" time="Santos" habilidade="rapidez de raciocínio e inteligência na armação das jogadas."/>
+      <Tudo foto={f3} nome="Pituca" numero="21" posicao="Meio-campista" time="Santos" habilidade="rapidez de raciocínio e inteligência."/>
     </div>
     </div>
   )
--- src/views/Tudo.jsx
+++ src/views/Tudo.jsx
@@ -1,20 +1,21 @@
 import icon from '../img/icon.png'
-const Tudo = ({foto , nome ,numero, posicao , habilidade}) => {
+const Tudo = ({foto , nome ,numero, posicao , habilidade , time}) => {
     return(
         <div className='div'>
+                    <img className='img' src={foto} alt="foto"/>
             <div>
-            <img className='img' src={foto} alt="foto"/>
-            </div>
-            <div>
-            <div className='texto'>
-            <h1 >{nome}</h1>
-            <h1 className='h1'>#{numero}</h1>
-            </div>
-            <h1>{posicao}</h1>
-            <div className='texto2'>
-                <img className='icon' src={icon} alt="" />
-                <h1 className='h1'>Habilidade:{habilidade}</h1>
-            </div>
+                <div className='text'>
+
+                <div className='texto'>
+                    <h1 >{nome}</h1>
+                    <h1 className='h1'>#{numero}</h1>
+                </div>
+             <h1>{posicao} - {time}</h1>
+             </div>
+                <div className='texto2'>
+                   <img className='icon' src={icon} alt="" />
+                   <h1 className='h1-baixo'>Habilidade:{habilidade}</h1>
+                </div>
             </div>
         </div>
     )
